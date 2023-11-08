6d88bd1a223ca0c1d06b5e908937951b83442923
Kenneth <alcanamay@gmail.com>
2023-11-08 15:34:41 -0600

    FireBaseConfiguración

---
+++ js/modules/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyALdQ6LTiJ0jGD7Vv0xo7kTn82eBaJ6Tak",
+authDomain: "proyecto-2801e.firebaseapp.com",
+databaseURL: "https://proyecto-2801e-default-rtdb.firebaseio.com",
+projectId: "proyecto-2801e",
+storageBucket: "proyecto-2801e.appspot.com",
+messagingSenderId: "416017585473",
+appId: "1:416017585473:web:70636f4a15aca63d887dc0"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
