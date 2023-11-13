a86d679aa87a81b2d964d1754a6a31386ceb51fa
Kenneth <alcanamay@gmail.com>
2023-11-13 00:53:08 -0600

    Autentificacion con Google

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 import {app} from "./modules/firebaseConfig.js";
 import { firebaseCRUD } from "./modules/firebaseDataBase.js";
 import { authEmail } from "./modules/firebaseAuthEmail.js";
+import { authGoogle } from "./modules/firebaseAuthGoogle.js";
 
+authGoogle(app);
 firebaseCRUD(app);
 authEmail(app);
