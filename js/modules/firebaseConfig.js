ae3a28accca71a2d5cf16cdbc77badf849536474
Kenneth <alcanamay@gmail.com>
2023-11-13 00:45:39 -0600

    Probando Autenticar Email

--- js/modules/firebaseConfig.js
+++ js/modules/firebaseConfig.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyALdQ6LTiJ0jGD7Vv0xo7kTn82eBaJ6Tak",
-authDomain: "proyecto-2801e.firebaseapp.com",
-databaseURL: "https://proyecto-2801e-default-rtdb.firebaseio.com",
-projectId: "proyecto-2801e",
-storageBucket: "proyecto-2801e.appspot.com",
-messagingSenderId: "416017585473",
-appId: "1:416017585473:web:70636f4a15aca63d887dc0"
+    apiKey: "AIzaSyDw6M8q32E-PEKG8BRo75ZOEtc8wXfLDrM",
+    authDomain: "programensostudiosweb.firebaseapp.com",
+    databaseURL: "https://programensostudiosweb-default-rtdb.firebaseio.com",
+    projectId: "programensostudiosweb",
+    storageBucket: "programensostudiosweb.appspot.com",
+    messagingSenderId: "101780936855",
+    appId: "1:101780936855:web:a6f831e371e53c4457b1d2"
 };
 
 // Initialize Firebase
--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import {app} from "./modules/firebaseConfig.js";
 import { firebaseCRUD } from "./modules/firebaseDataBase.js";
+import { authEmail } from "./modules/firebaseAuthEmail.js";
 
 firebaseCRUD(app);
+authEmail(app);
