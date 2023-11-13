a38dfde172fc1d1c819c51c4f44935e7b781f1a1
Kenneth <alcanamay@gmail.com>
2023-11-13 00:08:06 -0600

    Prueba Firebase

--- js/modules/firebaseDataBase.js
+++ js/modules/firebaseDataBase.js
@@ -10,5 +10,83 @@ import {
 
 export function firebaseCRUD(app){
     const d = document;
+    db = getDatabase(app),
+    refPrice = ref(db, "cities"),
+    $form = d.getElementById("form-price"),
+    $table = d.getElementById("table-price"),
+    $template = d.getElementById("template-price").content,
+    $fragment = d.createDocumentFragment();
+
+    function renderPrice() {
+        onValue(refPrice, (snapshot) => {
+          //limpiar el contenido de la tabla
+        $table.querySelector("tbody").innerHTML = "";
     
+        snapshot.forEach((el) => {
+            //console.log(el);
+            //console.log(el.key);
+            //console.log(el.val());
+    
+            let values = el.val(),
+            key = el.key;
+    
+            $template.querySelector(".key-id").id = key;
+            $template.querySelector(".key").innerText = key;
+            $template.querySelector(".product").innerText = values.product;
+            $template.querySelector(".price").innerText = values.price;
+    
+            let $clone = d.importNode($template, true);
+            $fragment.appendChild($clone);
+        });
+    
+        $table.querySelector("tbody").appendChild($fragment);
+        });
+    }
+
+    d.addEventListener("DOMContentLoaded", (e) => renderPrice());
+
+    $form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let key = e.target.key,
+    values = {
+        product: $form.product.value,
+        price: $form.price.value,
+    };
+
+    if (!key.value) {
+      //Insertar
+    push(ref(db, "price"), values);
+    } else {
+      //Actualizar
+    update(ref(db), {
+        ["/price/" + key.value]: values,
+    });
+    }
+
+    $form.reset();
+});
+
+d.addEventListener("click", (e) => {
+    if (!e.target.matches(".edit") && !e.target.matches(".delete"))
+    return false;
+
+    if (e.target.matches(".edit")) {
+      //alert("Editar");
+      //console.log(e.target.parentElement.parentElement.id);
+    let $parent = e.target.parentElement.parentElement;
+    $form.product.value = $parent.querySelector(".product").innerText;
+    $form.price.value = $parent.querySelector(".price").innerText;
+    $form.key.value = $parent.id;
+    }
+
+    if (e.target.matches(".delete")) {
+      //alert("Eliminar");
+    let key = e.target.parentElement.parentElement.id,
+        deleteId = confirm(`¿Estás seguro de eliminar el id ${key}?`);
+
+    if (deleteId) remove(ref(db, `/price/${key}`));
+    }
+});
 }
+
---
+++ js/script.js
@@ -0,0 +1,4 @@
+import {app} from "./modules/firebaseConfig.js";
+import { firebaseCRUD } from "./modules/firebaseDataBase.js";
+
+firebaseCRUD(app);
