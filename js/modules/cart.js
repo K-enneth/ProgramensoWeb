444a1874e27c87f49be138b0e5d57a4e930d68ec
KaryVelle <105183305+KaryVelle@users.noreply.github.com>
2023-11-15 16:01:54 -0600

    Debugs para pago

--- js/modules/cart.js
+++ js/modules/cart.js
@@ -1,20 +1,34 @@
-let cartCount = 0;
 
- export function buyProduct(productId) {
-   
-    cartCount++;
-    document.getElementById('cart-count').innerText = cartCount;
-    console.log(`Producto ${productId} agregado al carrito.`);
-}
 
-export function toggleCart() {
+
+ function toggleCart() {
     
-    let cartSection = document.getElementById('cart');
-    cartSection.style.display = (cartSection.style.display === 'block') ? 'none' : 'block';
+    console.log("Carrito clickeado");
 }
 
-export function showCheckout() {
+
+ function buyProduct(productId) {
    
-    document.getElementById('cart').style.display = 'none';
-    document.getElementById('checkout').style.display = 'block';
+    console.log("Producto comprado: " + productId);
 }
+
+
+ function showCheckout() {
+
+    console.log("Mostrar pantalla de pago");
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    document.getElementById('cart').addEventListener('click', toggleCart);
+
+
+    var productButtons = document.querySelectorAll('.product-card button');
+    productButtons.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            buyProduct(index + 1);
+        });
+    });
+
+    document.getElementById('checkout-btn').addEventListener('click', showCheckout);
+});
