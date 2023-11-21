d2e1f50567c64a0ca1683f45fa4aec54a3eddb8c
KaryVelle <105183305+KaryVelle@users.noreply.github.com>
2023-11-21 16:00:50 -0600

    foto

--- js/modules/cart.js
+++ js/modules/cart.js
@@ -1,34 +1,59 @@
-
-
-
- function toggleCart() {
-    
-    console.log("Carrito clickeado");
-}
-
-
- function buyProduct(productId) {
-   
-    console.log("Producto comprado: " + productId);
-}
-
-
- function showCheckout() {
-
-    console.log("Mostrar pantalla de pago");
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    
-    document.getElementById('cart').addEventListener('click', toggleCart);
-
-
-    var productButtons = document.querySelectorAll('.product-card button');
-    productButtons.forEach(function (button, index) {
-        button.addEventListener('click', function () {
-            buyProduct(index + 1);
+const cart = {
+    items: [],
+  
+    addToCart: function (productId) {
+      const product = {
+        id: productId,
+        name: `Producto ${productId}`,
+        price: 1000,
+      };
+  
+      this.items.push(product);
+      updateCartCount();
+      updateCartDisplay();
+      showAddedToCartMessage();
+    },
+  
+    showCart: function () {
+      const cartItemsContainer = document.getElementById('cart-items');
+      cartItemsContainer.innerHTML = '';
+  
+      if (this.items.length === 0) {
+        cartItemsContainer.innerHTML = '<p>El carrito está vacío.</p>';
+      } else {
+        this.items.forEach((item) => {
+          const cartItem = document.createElement('div');
+          cartItem.innerHTML = `<p>${item.name} - $${item.price}</p>`;
+          cartItemsContainer.appendChild(cartItem);
         });
-    });
-
-    document.getElementById('checkout-btn').addEventListener('click', showCheckout);
-});
+      }
+    },
+  };
+  
+  function updateCartCount() {
+    const cartCountElement = document.getElementById('cart-count');
+    cartCountElement.textContent = cart.items.length;
+  }
+  
+  function updateCartDisplay() {
+    const cartItemsContainer = document.getElementById('cart-items');
+    cart.showCart();
+  }
+  
+  function toggleCart() {
+    console.log("Carrito clickeado");
+  }
+  
+  function buyProduct(productId) {
+    console.log("Producto comprado: " + productId);
+    cart.addToCart(productId);
+  }
+  
+  function showCheckout() {
+    console.log("Mostrar pantalla de pago");
+  }
+  
+  function showAddedToCartMessage() {
+    alert("Producto añadido al carrito");
+  }
+  
