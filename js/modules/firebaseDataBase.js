857980e7ec882cd71bde36d0453ab33579cc2194
Kenneth <alcanamay@gmail.com>
2023-11-22 23:19:00 -0600

    Arreglando firebase

--- js/modules/firebaseDataBase.js
+++ js/modules/firebaseDataBase.js
@@ -11,8 +11,7 @@ import {
 export function firebaseCRUD(app){
     const d = document,
     db = getDatabase(app),
-    refPrice = ref(db, "prices"),
-    $form = d.getElementById("form-price"),
+    refPrice = ref(db, "price"),
     $table = d.getElementById("table-price"),
     $template = d.getElementById("template-price").content,
     $fragment = d.createDocumentFragment();
@@ -24,8 +23,6 @@ export function firebaseCRUD(app){
         snapshot.forEach((el) => {
             let values = el.val(),
             key = el.key;
-            $template.querySelector(".key-id").id = key;
-            $template.querySelector(".key").innerText = key;
             $template.querySelector(".product").innerText = values.product;
             $template.querySelector(".price").innerText = values.price;
     
@@ -39,45 +36,5 @@ export function firebaseCRUD(app){
 
     d.addEventListener("DOMContentLoaded", (e) => renderPrice());
 
-    $form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let key = e.target.key,
-    values = {
-        product: $form.product.value,
-        price: $form.price.value,
-    };
-
-    if (!key.value) {
-    push(ref(db, "price"), values);
-    } else {
-    update(ref(db), {
-        ["/price/" + key.value]: values,
-    });
-    }
-
-    $form.reset();
-});
-
-d.addEventListener("click", (e) => {
-    if (!e.target.matches(".edit") && !e.target.matches(".delete"))
-    return false;
-
-    if (e.target.matches(".edit")) {
-    alert("Editar");
-    let $parent = e.target.parentElement.parentElement;
-    $form.product.value = $parent.querySelector(".product").innerText;
-    $form.price.value = $parent.querySelector(".price").innerText;
-    $form.key.value = $parent.id;
-    }
-
-    if (e.target.matches(".delete")) {
-    alert("Eliminar");
-    let key = e.target.parentElement.parentElement.id,
-        deleteId = confirm(`¿Estás seguro de eliminar el id ${key}?`);
-
-    if (deleteId) remove(ref(db, `/price/${key}`));
-    }
-});
 }
 
