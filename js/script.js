037210c0eeb93d2ae8933cf7be9cb6389d9f3c12
Kenneth <alcanamay@gmail.com>
2023-11-23 11:18:29 -0600

    añadiendo log in de forma primitva al home

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ import { firebaseCRUD } from "./modules/firebaseDataBase.js";
 import { authEmail } from "./modules/firebaseAuthEmail.js";
 import { authGoogle } from "./modules/firebaseAuthGoogle.js";
 
-authGoogle(app);
-firebaseCRUD(app);
 authEmail(app);
 
 document.addEventListener('DOMContentLoaded', function () {
