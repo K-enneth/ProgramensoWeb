b59be332632f1d1e686d29ca405d1f7aa261d75f
Kenneth <alcanamay@gmail.com>
2023-11-26 18:23:37 -0600

    CARRITO ARREGLADO

--- js/modules/cart.js
+++ js/modules/cart.js
@@ -1,55 +1,68 @@
+
 const cart = {
-    items: [],
-  
-    addToCart: function (productId) {
-      const product = {
-        id: productId,
-        name: `Producto ${productId}`,
-        price: 1000,
-      };
-  
+  items: [],
+
+  addToCart: function (product) {
       this.items.push(product);
       updateCartCount();
       updateCartDisplay();
       showAddedToCartMessage();
-    },
-  
-    showCart: function () {
+  },
+
+  showCart: function () {
       const cartItemsContainer = document.getElementById('cart-items');
       cartItemsContainer.innerHTML = '';
-  
+
       if (this.items.length === 0) {
-        cartItemsContainer.innerHTML = '<p>El carrito está vacío.</p>';
+          cartItemsContainer.innerHTML = '<p>El carrito está vacío.</p>';
       } else {
-        this.items.forEach((item) => {
-          const cartItem = document.createElement('div');
-          cartItem.innerHTML = `<p>${item.name} - $${item.price}</p>`;
-          cartItemsContainer.appendChild(cartItem);
-        });
+          this.items.forEach((item) => {
+              const cartItem = document.createElement('div');
+              cartItem.innerHTML = `<p>${item.name} - $${item.price}</p>`;
+              cartItemsContainer.appendChild(cartItem);
+          });
       }
-    },
+  },
+};
+
+function updateCartCount() {
+  const cartCountElement = document.getElementById('cart-count');
+  cartCountElement.textContent = cart.items.length;
+}
+
+function updateCartDisplay() {
+  const cartItemsContainer = document.getElementById('cart-items');
+  cart.showCart();
+}
+
+function buyProduct(button) {
+  // Obtener el contenedor del producto
+  let productContainer = button.closest('.product-card');
+
+  // Obtener el nombre y otros detalles del producto desde el contenedor
+  let productName = productContainer.querySelector('h3').innerText;
+  let productPriceText = productContainer.querySelector('p').innerText;
+  
+  // Extraer el precio numérico del texto (por ejemplo, "Precio: $1000" -> 1000)
+  let productPrice = parseInt(productPriceText.replace("Precio: $", ""));
+
+  // Crear un objeto de producto
+  const product = {
+      name: productName,
+      price: productPrice,
   };
+
+  // Añadir el producto al carrito
+  cart.addToCart(product);
   
-  function updateCartCount() {
-    const cartCountElement = document.getElementById('cart-count');
-    cartCountElement.textContent = cart.items.length;
-  }
-  
-  function updateCartDisplay() {
-    const cartItemsContainer = document.getElementById('cart-items');
-    cart.showCart();
-  }
-  
-  function toggleCart() {
-    console.log("Carrito clickeado");
-  }
-  
-  function buyProduct(productId) {
-    console.log("Producto comprado: " + productId);
-    cart.addToCart(productId);
-  }
-  
-  function showAddedToCartMessage() {
-    alert("Producto añadido al carrito");
-  }
+  // Mostrar el mensaje de producto añadido al carrito
+  showAddedToCartMessage();
+}
+
+function showAddedToCartMessage() {
+  alert("Producto añadido al carrito");
+}
+
+// Resto del código según tus necesidades, como la inicialización de Firebase y el manejo de eventos DOM.
+
   
